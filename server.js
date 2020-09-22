953fa6d8bbf18fabe1889fe7476847157350401b
Justin Dionisio <justindionisio21@yahoo.com>
2020-09-21 18:07:01 -0700

    Initial Commit. Set up server, schema model, and input validation

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const app = express();
+
+// Bodyparser middleware
+// app.use(
+// 	bodyParser.urlencoded({
+// 		extended: false
+// 	})
+// );
+// app.use(bodyParser.json());
+// app.use(express.json());
+
+// DB Config
+const db = require('./config/keys').mongoURI;
+
+// Connect to MongoDB
+mongoose
+	.connect(db, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+		useFindAndModify: false
+	})
+	.then(() => console.log('MongoDB successfully connected'))
+	.catch((err) => console.log(err));
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Server up and running!`));

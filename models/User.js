443977fb20ce4d365501db0623ddb6e8e38d6b76
Justin Dionisio <justindionisio21@yahoo.com>
2020-09-25 16:54:08 -0700

    Set up backend for registration/login

--- models/User.js
+++ models/User.js
@@ -1,6 +1,30 @@
+// const mongoose = require('mongoose');
+// const Schema = mongoose.Schema;
+
+// const UserSchema = new Schema({
+// 	name: {
+// 		type: String,
+// 		required: true
+// 	},
+// 	email: {
+// 		type: String,
+// 		required: true
+// 	},
+// 	password: {
+// 		type: String,
+// 		required: true
+// 	},
+// 	date: {
+// 		type: Date,
+// 		default: Date.now
+// 	}
+// });
+// module.exports = User = mongoose.model('users', UserSchema);
+
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+// Create Schema
 const UserSchema = new Schema({
 	name: {
 		type: String,
@@ -8,15 +32,17 @@ const UserSchema = new Schema({
 	},
 	email: {
 		type: String,
-		required: true
+		required: true,
+		unique: true
 	},
 	password: {
 		type: String,
 		required: true
 	},
-	date: {
+	register_date: {
 		type: Date,
 		default: Date.now
 	}
 });
+
 module.exports = User = mongoose.model('users', UserSchema);
--- server.js
+++ server.js
@@ -1,16 +1,18 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const passport = require('passport');
 const app = express();
 
+const users = require('./routes/api/users');
+
 // Bodyparser middleware
-// app.use(
-// 	bodyParser.urlencoded({
-// 		extended: false
-// 	})
-// );
-// app.use(bodyParser.json());
-// app.use(express.json());
+app.use(
+	bodyParser.urlencoded({
+		extended: false
+	})
+);
+app.use(bodyParser.json());
 
 // DB Config
 const db = require('./config/keys').mongoURI;
@@ -18,11 +20,20 @@ const db = require('./config/keys').mongoURI;
 // Connect to MongoDB
 mongoose
 	.connect(db, {
-		useNewUrlParser: true,
 		useUnifiedTopology: true,
-		useFindAndModify: false
+		useNewUrlParser: true,
+		useCreateIndex: true
 	})
 	.then(() => console.log('MongoDB successfully connected'))
 	.catch((err) => console.log(err));
+
+// Passport middleware
+app.use(passport.initialize());
+
+// Passport config
+require('./config/passport')(passport);
+// Routes
+app.use('/api/users', users);
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server up and running!`));
